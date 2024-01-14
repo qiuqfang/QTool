7f15adc9d1cded6e5447a175496278972798fc32
Quincy Qiu <qiuqfang@outlook.com>
2024-01-14 19:26:44 +0800

    fix: change click position

--- components/FileUpload.vue
+++ components/FileUpload.vue
@@ -2,28 +2,26 @@
 const props = defineProps<{ accept?: string }>();
 const emits = defineEmits(["upload"])
 const { files, open, reset, onChange } = useFileDialog({
-    accept: props.accept ?? "*", // Set to accept only image files
-    directory: false, // Select directories instead of files if set true
+  accept: props.accept ?? "*", // Set to accept only image files
+  directory: false, // Select directories instead of files if set true
 })
 onChange((files) => {
-    if (files!?.length > 0) {
-        emits("upload", files)
-        reset();
-    }
+  if (files!?.length > 0) {
+    emits("upload", files)
+    reset();
+  }
 
 })
 </script>
 
 <template>
   <div class="file-upload">
-    <div
-      class="file-upload-wrap flex justify-center items-center"
-      @click="() => {
-        open()
-      }"
-    >
+    <div class="file-upload-wrap flex justify-center items-center">
       <div
         class="w-[500px] h-[300px] text-gray-400 hover:text-gray-500 border-dashed border-gray-400 hover:border-gray-500 transition-all  rounded flex flex-col justify-center items-center cursor-pointer "
+        @click="() => {
+          open()
+        }"
       >
         <i class="i-material-symbols-drive-folder-upload text-[32px]" />
         <div class="text-[14px]">
